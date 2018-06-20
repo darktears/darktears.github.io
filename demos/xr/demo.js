e7b6b374532257c6a9ab2d2228a5a6115ac32b02
Alexis Menard <alexis.menard@intel.com>
2018-06-20 12:27:18 -0700

    Add mouse + keyboard controls when there is no VR system available.

--- demos/xr/demo.js
+++ demos/xr/demo.js
@@ -43,6 +43,7 @@ class Demo {
     this._boxMaterial;
     this._boxMaterialGray;
     this._container = document.querySelector('#container');
+    this._startMessage = document.querySelector('#start');
 
     this.clearContainer();
     this.createRenderer();
@@ -70,6 +71,12 @@ class Demo {
     this._lasers = [];
     this._cursors = [];
     this._activeCursors = 0;
+    this._prevTime = performance.now();
+    this._velocity = new THREE.Vector3();
+    this._moveForward = false;
+    this._moveBackward = false;
+    this._moveRight = false;
+    this._moveLeft = false;
     this._checkForXR();
   }
 
@@ -77,6 +84,8 @@ class Demo {
     navigator.xr.requestDevice().then(device => {
       this._onXRAvailable(device);
     }, err => {
+      // Let's provide a mouse/keyboard based interaction.
+      this._enableKeyboardMouse();
       if (err.message === 'NotFoundError') {
         // No XRDevices available.
         console.error('No XR devices available :', err);
@@ -85,6 +94,78 @@ class Demo {
         console.error('Requesting XR device failed :', err);
       }
     });
+  }
+
+  _enableKeyboardMouse() {
+    if (!this._hasPointerLock())
+      return;
+    this._controls = new THREE.PointerLockControls(this._camera);
+    this._scene.add(this._controls.getObject());
+    this._controls.getObject().position.y = 1;
+    this._camera.lookAt(new THREE.Vector3(0, 0, 1));
+    // Hook pointer lock state change events
+    document.addEventListener('pointerlockchange', _ => { this._pointerLockChanged() }, false );
+    document.addEventListener('mozpointerlockchange', _ => { this._pointerLockChanged() }, false );
+    document.addEventListener('webkitpointerlockchange', _ => { this._pointerLockChanged() }, false );
+    document.addEventListener('keydown', event => { this._onKeyDown(event) }, false );
+    document.addEventListener('keyup', event => { this._onKeyUp(event) }, false );
+
+    document.body.addEventListener( 'click', _ => {
+      // Ask the browser to lock the pointer
+      document.body.requestPointerLock = document.body.requestPointerLock || document.body.mozRequestPointerLock || document.body.webkitRequestPointerLock;
+      document.body.requestPointerLock();
+    }, false);
+  }
+
+  _onKeyDown(event) {
+    switch ( event.keyCode ) {
+      case 38: // up
+      case 87: // w
+        this._moveForward = true;
+        break;
+      case 37: // left
+      case 65: // a
+        this._moveLeft = true; break;
+      case 40: // down
+      case 83: // s
+        this._moveBackward = true;
+        break;
+      case 39: // right
+      case 68: // d
+        this._moveRight = true;
+        break;
+    }
+  }
+
+  _onKeyUp(event) {
+    switch( event.keyCode ) {
+      case 38: // up
+      case 87: // w
+        this._moveForward = false;
+        break;
+      case 37: // left
+      case 65: // a
+        this._moveLeft = false;
+        break;
+      case 40: // down
+      case 83: // s
+        this._moveBackward = false;
+        break;
+      case 39: // right
+      case 68: // d
+        this._moveRight = false;
+        break;
+    }
+  }
+
+  _pointerLockChanged() {
+    if (document.pointerLockElement === document.body || document.mozPointerLockElement === document.body || document.webkitPointerLockElement === document.body) {
+      this._controls.enabled = true;
+      this._startMessage.style.display = 'none';
+    } else {
+      this._startMessage.style.display = 'flex';
+      this._controls.enabled = false;
+    }
   }
 
   _checkMagicWindowSupport() {
@@ -325,6 +406,7 @@ class Demo {
     requestAnimationFrame(this._update);
     if (this._magicWindowCanvas)
       this._activateMagicWindow(this._magicWindowCanvas.getContext('xrpresent'));
+    this._camera.lookAt(new THREE.Vector3(0, 0, 1));
   }
 
   async _activateMagicWindow (ctx) {
@@ -386,6 +468,38 @@ class Demo {
     };
   }
 
+  _updatePosition() {
+    let time = performance.now();
+    let delta = (time - this._prevTime) / 1000;
+
+    // Decrease the velocity.
+    this._velocity.x -= this._velocity.x * 10.0 * delta;
+		this._velocity.z -= this._velocity.z * 10.0 * delta;
+
+    let controls_yaw = this._controls.getObject();
+
+    if (this._moveForward) this._velocity.z += 100.0 * delta;
+    if (this._moveBackward) this._velocity.z -= 100.0 * delta;
+    if (this._moveLeft) this._velocity.x += 100.0 * delta;
+    if (this._moveRight) this._velocity.x -= 100.0 * delta;
+
+    controls_yaw.translateX(this._velocity.x * delta);
+		controls_yaw.translateY(this._velocity.y * delta);
+    controls_yaw.translateZ(this._velocity.z * delta);
+
+    if (controls_yaw.position.z > 6)
+      controls_yaw.position.z = 6;
+    if (controls_yaw.position.z < -2)
+      controls_yaw.position.z = -2;
+
+    if (controls_yaw.position.x > 4)
+      controls_yaw.position.x = 4;
+    if (controls_yaw.position.x < -4)
+      controls_yaw.position.x = -4;
+
+    this._prevTime = time;
+  }
+
   _render (timestamp, xrFrame) {
     if (!this._xrSession) {
       // Ensure that we switch everything back to auto for non-VR mode.
@@ -393,6 +507,9 @@ class Demo {
       this._renderer.setViewport(0, 0, this._width, this._height);
       this._renderer.autoClear = true;
       this._scene.matrixAutoUpdate = true;
+      if (this._controls && this._controls.enabled) {
+        this._updatePosition();
+      }
       this._renderer.render(this._scene, this._camera);
       return requestAnimationFrame(this._update);
     }
@@ -559,6 +676,11 @@ class Demo {
     var check = false;
     (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
     return check;
+  }
+
+  _hasPointerLock() {
+    let havePointerLock = 'pointerLockElement' in document || 'mozPointerLockElement' in document || 'webkitPointerLockElement' in document;
+    return havePointerLock;
   }
 }
 
